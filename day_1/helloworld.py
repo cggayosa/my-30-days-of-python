b47dffc331fb3a1a1ca5ff0dfe0bdf8939ad8fa6
cggayosa <203806905+cggayosa@users.noreply.github.com>
2026-07-30 00:57:42 +0800

    Finished Day 1, Exercise Level 2

---
+++ day_1/helloworld.py
@@ -0,0 +1,45 @@
+# EXERCISE:
+# Day 1, Exercise Level 2 (different run)
+
+# 1. Check the python version you are using
+print('Python version: 3.14.4')
+
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+# Addition (+)
+print('Addition: ', 3+4)
+# Subtraction (-)
+print('Subtraction: ', 3-4)
+# Multiplication (*)
+print('Multiplication: ', 3*4)
+# Division (/)
+print('Division: ', 3/4)
+# Exponential (**)
+print('Exponential: ', 3**4)
+
+# 3. Write strings on the python interactive shell. The strings are the following
+# Your name
+print('Caitlin')
+# Your family name
+print('Gayosa')
+# Your country
+print('Philippines')
+# I am enjoying 30 days of python
+print('I am enjoying 30 days of python ')
+
+# 4. Check the data types of the following data: 
+# 10
+print(type(10))
+# 9.8
+print(type(9.8))
+# 3.14
+print(type(3.14))
+# 4 - 4j
+print(type(4-4j))
+# ['Asabeneh', 'Python', 'Finland']
+print(type(['Asabeneh', 'Python', 'Finland']))
+# Your name
+print(type('Caitlin'))
+# Your family name
+print(type('Gayosa'))
+# Your country
+print(type('Philippines'))
