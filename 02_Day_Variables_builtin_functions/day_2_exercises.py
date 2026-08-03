1223be5a110774be423d549c556f1951db810134
cggayosa <203806905+cggayosa@users.noreply.github.com>
2026-08-03 23:40:43 +0800

    Updated Day 2 Exercise

--- 02_Day_Variables_builtin_functions/day_2_exercises.py
+++ 02_Day_Variables_builtin_functions/day_2_exercises.py
@@ -58,5 +58,6 @@ area_of_circle = (355 / 113) *(radius ** 2)
 circum_of_circle = 2 * (355/113) * radius
 # Take radius as user input and calculate the area.
 
+
 # Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
 # Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
